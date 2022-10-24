8a320186c0e9005f24f61678e28799a51ca6c09a
iamriajul <kmriajulislami@gmail.com>
2022-10-24 17:59:32 +0600

    style: flutter format.

--- lib/flutter_circle_flags_svg.dart
+++ lib/flutter_circle_flags_svg.dart
@@ -8,7 +8,11 @@ class CircleFlagSvg extends StatelessWidget {
   final String code;
   final double? size;
 
-  const CircleFlagSvg({required this.code, this.size = 24, super.key,});
+  const CircleFlagSvg({
+    required this.code,
+    this.size = 24,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
